3797e67b0af043176658898f3df7398e3bd2f92e
romalukin <romalukin@gmail.com>
2020-05-10 19:51:12 +0300

    upgraded code, now it's a bit pretty

--- krepko-test.py
+++ krepko-test.py
@@ -26,40 +26,51 @@ def get_catalog(url_link: str) -> dict:
     catalog = dict(zip(category_list, urls))
     return catalog
 
-def  get_products(catalog_name: str, url_link: str) -> dict:
+def  get_products(catalog_name: str, url_link: str) -> list:
+    url_home = 'https://krepkoshop.com/category'
+    product_list = []
     #get the html of site 
     result = requests.get(url_link)
     src = result.content
     #make the content good to work with beautifulsoup
     soup = BeautifulSoup(src, 'lxml')
 
-    for ul_tag in soup.find_all('ul'):
-        if ul_tag['class'] == ['li3', 'product-list', 'thumbs']:
-            li_tag_list = ul_tag.find_all('li')
 
-    for li_tag in li_tag_list:            
+    catalog = soup.find_all('li', {'itemtype': 'http://schema.org/Product'})
+
+    for card in catalog:            
         product = {
             'name': '',
             'old_price': 0,
             'sale': 0,
             'price': 0,
             'description': '',
-            'category': '',
+            'category': catalog_name,
             'url': ''
         }
-        product['name'] = li_tag.find('h5').find('span').string.replace('\n', ' ')
-        product['url'] = url_link + str(li_tag.find('h5').find('a')['href'].lstrip('/'))
-        product['description'] = li_tag.find('meta')['content']
-        product['price'] = int(li_tag.find('span', {'class':'price nowrap'}).contents[0].strip(' руб.').replace(' ',''))
-        if li_tag.find('span', {'class':'compare-at-price nowrap'}):
-            product['old_price'] = int(li_tag.find('span', {'class':'compare-at-price nowrap'}).string.strip(' руб.').replace(' ',''))
+        product['name'] = card.find('h5', {'itemprop': 'name'}).find('span').string.replace('\n', ' ')
+        product['url'] = url_home + str(card.find('a', {'class': 'product-name'})['href'])
+        product['description'] = card.find('meta', {'itemprop': 'description'})['content']
+        product['price'] = int(card.find('span', {'class':'price nowrap'}).contents[0].strip(' руб.').replace(' ',''))
+        if card.find('span', {'class':'sale-compare-block'}):
+            product['old_price'] = int(card.find('span', {'class':'compare-at-price nowrap'}).string.strip(' руб.').replace(' ',''))
+            product['sale'] = int(card.find('span', {'class':'sale-compare-block'}).contents[1].string.lstrip(' (-').rstrip('%)'))
         else:
             product['old_price'] = product['price']   
+        product_list.append(product)
+    return product_list
 
-    return 0
 
+product_list = []
 #krepko site
 catalog = get_catalog("https://krepkoshop.com/category/")
 for category in catalog:
-    get_products(category, catalog[category])
+    product_list.append(get_products(category, catalog[category]))
 pprint(catalog)
+
+with open('C:/Users/romal/Documents/github/krepko/test.txt', 'w', encoding='utf-8') as f:
+    for category in product_list:
+        for card in category:
+            f.write('-----\n')
+            for field in card:
+                f.write('{}: {}\n'.format(field, card[field]))
