19a8a93f10d456a8cdb1fce1e1d9cc78355e4d1d
romalukin <romalukin@gmail.com>
2020-05-12 16:49:33 +0300

    commit

--- krepko_bot.py
+++ krepko_bot.py
@@ -6,6 +6,7 @@ import krepko_web_scraper
 
 def compare(old_product_list: list, new_product_list: list) -> list:
     compare_list = []
+    compare_string = ''
     for new_card in new_product_list:
         for old_card in old_product_list:
             if new_card['name'] == old_card['name']:
@@ -15,11 +16,17 @@ def compare(old_product_list: list, new_product_list: list) -> list:
                         'old_price': old_card['price'],
                         'price': new_card['price'],
                         'category': new_card['category'],
-                        'url': new_card['category']
+                        'url': new_card['url']
                     }
                     compare_list.append(product)
     compare_list = [dict(t) for t in {tuple(card.items()) for card in compare_list}]
-    return compare_list
+    for card in compare_list:
+        compare_string = compare_string + '''наименование: {}\n
+                                             старая цена: {}\n
+                                             цена: {}\n
+                                             категория: {}\n
+                                             ссылка: {}\n'''.format(card['name'], card['old_price'], card['price'], card['category'], card['url']) 
+    return compare_string
 	
 def bot_sendtext(bot_message):
 	### Send text message
@@ -35,4 +42,5 @@ print(files)
 with open(files[0], encoding='utf-8') as old, open(files[1], encoding='utf-8') as new:
     old_product_list = json.load(old)
     new_product_list = json.load(new)
-bot_sendtext('Hello world!')
+compare_string = compare(old_product_list, new_product_list) 
+bot_sendtext(compare_string)
